c4596cefdeb214644d7192316dd52a822a39d45c
Rafael Marinho <rafael.marinho07@gmail.com>
2020-02-13 22:27:32 -0300

    Adicionado validações após tentar submeter o formulario e após deixar o campo consulta cep

--- src/app/modules/consulta-cep/busca/busca.component.ts
+++ src/app/modules/consulta-cep/busca/busca.component.ts
@@ -6,17 +6,17 @@ import { ViaCepService } from "app/core/services/via-cep.service";
     selector: 'app-busca',
     template: `
         <div>
-            <form [formGroup]="buscaForm" (ngSubmit)="consultaCep()">
+            <form #form="ngForm" [formGroup]="buscaForm" (ngSubmit)="consultaCep()">
                 <h3>Consultar</h3>
                 busca: <input type="text" formControlName="cep">
                 <input type="submit" value="Consultar">
                 <helper-text 
                     [value]="'Preencha todos os campos'"
-                    *ngIf="buscaForm.get('cep').errors?.required">
+                    *ngIf="buscaForm.get('cep').errors?.required && (form.submitted || buscaForm.get('cep').touched)">
                 </helper-text>
                 <helper-text 
                     [value]="'Complete todos os digitos'"
-                    *ngIf="buscaForm.get('cep').errors?.minlength">
+                    *ngIf="buscaForm.get('cep').errors?.minlength  && (form.submitted || buscaForm.get('cep').touched)">
                 </helper-text>
             </form>
         </div>
@@ -47,12 +47,13 @@ export class BuscaComponent implements OnInit {
     consultaCep(){
         let cep = this.buscaForm.get('cep').value;
 
-        this.viaCep.getCep(cep).subscribe((result) => {
-            console.log(result);
-        },
-        (error) => {
-            console.log(error);
-        })
-        
+        if(this.buscaForm.valid && !this.buscaForm.pending){
+            this.viaCep.getCep(cep).subscribe((result) => {
+                console.log(result);
+            },
+            (error) => {
+                console.log(error);
+            })
+        }
     }
 }
