d47fba320d979ab75dec451f40ee168966ee2e63
Rafael Marinho <rafael.marinho07@gmail.com>
2020-02-16 00:36:04 -0300

    Adicionado estilo do header e bootstrap grid

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -4,7 +4,13 @@ import { Component } from "@angular/core";
     selector: 'app-header',
     template: `
         <app-linear-progress></app-linear-progress>
-    `
+        <div class="header">
+            <div class="header__container">
+                <h1 class="header__logo">Labs Map</h1>
+            </div>
+        <div>
+    `,
+    styleUrls: ['./header.component.scss']
 })
 
 export class HeaderComponent {}
