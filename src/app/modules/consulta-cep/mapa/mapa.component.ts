deef0c24bcf6cb7fb8b01c87a956698fc13c0b88
Rafael Marinho <rafael.marinho07@gmail.com>
2020-02-16 01:07:16 -0300

    Alinhamento do mapa e criação do cabeçalho do mapa

--- src/app/modules/consulta-cep/mapa/mapa.component.ts
+++ src/app/modules/consulta-cep/mapa/mapa.component.ts
@@ -5,22 +5,33 @@ import { Location } from "../../../core/models/location";
 @Component({
     selector: 'app-mapa',
     template: `
-    <form (ngSubmit)="showLocation()">
-  <input type="text" [(ngModel)]="address" required [ngModelOptions]="{standalone: true}">
-  <button type="submit">Show Location</button>
-</form>
+    <!--form (ngSubmit)="showLocation()">
+      <input type="text" [(ngModel)]="address" required [ngModelOptions]="{standalone: true}">
+      <button type="submit">Show Location</button>
+    </form-->
+    <div class="map">
+      <div class="map__header">
+          <div class="map__address">
+            <h2>Rua Miguel Mentem</h2>
+            <p>Vila Guilherme</p>
+            <p>São Paulo - SP</p>
+            <p>02050-010</p>
+          </div>
+          <div class="map__close">X</div>
+      </div>
 
-<agm-map
-  [latitude]="location?.lat"
-  [longitude]="location?.lng"
-  [zoom]="8"
-  [disableDefaultUI]="false"
-  [zoomControl]="false">
-  <agm-marker 
-      [latitude]="location?.lat"
-      [longitude]="location?.lng">   
-  </agm-marker>
-</agm-map>
+      <agm-map
+        [latitude]="location?.lat"
+        [longitude]="location?.lng"
+        [zoom]="8"
+        [disableDefaultUI]="false"
+        [zoomControl]="false">
+        <agm-marker 
+            [latitude]="location?.lat"
+            [longitude]="location?.lng">   
+        </agm-marker>
+      </agm-map>
+    </div>
     `,
     styleUrls: ['./mapa.component.scss']
 })
